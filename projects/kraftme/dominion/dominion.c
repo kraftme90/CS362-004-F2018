4441a97445b568245a79e1426285b2ba10251f5d
Riley Kraft <kraftme@oregonstate.edu>
2018-10-09 14:03:52 -0400

    Refactored card code

--- projects/kraftme/dominion/dominion.c
+++ projects/kraftme/dominion/dominion.c
@@ -747,7 +747,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 			
     case gardens:
-      return -1;
+    	getGardens();
 			
     case mine:
       j = state->hand[currentPlayer][choice1];  //store card we will trash
@@ -1293,6 +1293,10 @@ int getSmithy(struct gameState *state, int currentPlayer, int handPos){
     //discard card from hand
     discardCard(handPos, currentPlayer, state, 0);
     return 0;
+}
+
+int getGardens(){
+	return -1;
 }
 
 int getSalvager(int choice1, struct gameState *state, int currentPlayer, int handPos){
