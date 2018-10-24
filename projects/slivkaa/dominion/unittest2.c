ce065511b5afd5b80c2158c1c18d19dd56725cac
Artem Slivka <aslivka0@gmail.com>
2018-10-24 13:56:14 -0400

    running tests on flip

--- projects/slivkaa/dominion/unittest2.c
+++ projects/slivkaa/dominion/unittest2.c
@@ -7,11 +7,12 @@
  */
 
 #include "testLib.h"
+#define TESTFUNC "numHandCards()"
 
 int main(){
 
     struct gameState states[5];
-    int i, retVal, expVal, numPasses = 0;
+    int i, actual, expected, numPasses = 0;
 
     for(i = 0; i < MAX_PLAYERS; i++){
         states[0].handCount[i] = 0;
@@ -39,45 +40,54 @@ int main(){
     states[4].whoseTurn = 0;
     states[4].handCount[states[4].whoseTurn] = 1;
   
-    retVal = numHandCards(&states[0]);
-    expVal = 1;
-    if(compareInts(expVal, retVal) == 0){
-        printf("numHandCards(): PASS player 0's handcount = 1\n");
+  	printf("----------------- Testing function: %s ----------------\n", TESTFUNC);  
+    actual = numHandCards(&states[0]);
+    expected = 1;
+    if(actual == expected){
+        printf("TEST 1: PASS player 0's handcount = 1\n");
+        printf("\thand count = %d, expected = %d\n", actual, expected);
         numPasses++;
     }
     else{
-        printf("numHandCards(): FAIL player 0's handcount != 1\n");
+        printf("TEST 1: FAIL player 0's handcount != 1\n");
+        printf("\thand count = %d, expected = %d\n", actual, expected);
     }
 
-    retVal = numHandCards(&states[1]);
-    expVal = 1;
-    if(compareInts(expVal, retVal) == 0){
-        printf("numHandCards(): PASS player 4's handcount = 1\n");
+    actual = numHandCards(&states[1]);
+    expected = 1;
+    if(actual == expected){
+        printf("TEST 2: PASS player 4's handcount = 1\n");
+        printf("\thand count = %d, expected = %d\n", actual, expected);
         numPasses++;
     }
     else{
-        printf("numHandCards(): FAIL player 4's handcount != 1\n");
+        printf("TEST 2: FAIL player 4's handcount != 1\n");
+        printf("\thand count = %d, expected = %d\n", actual, expected);
     }
 
-    retVal = numHandCards(&states[2]);
-    expVal = 20;
-    if(compareInts(expVal, retVal) == 0){
-        printf("numHandCards(): PASS player 1's handcount = 20\n");
+    actual = numHandCards(&states[2]);
+    expected = 20;
+    if(actual == expected){
+        printf("TEST 3: PASS player 1's handcount = 20\n");
+        printf("\thand count = %d, expected = %d\n", actual, expected);
         numPasses++;
     }
     else{
-        printf("numHandCards(): FAIL player 1's handcount != 20\n");
+        printf("TEST 3: FAIL player 1's handcount != 20\n");
+        printf("\thand count = %d, expected = %d\n", actual, expected);
     }
 
     numHandCards(&states[3]);
-    retVal = compareGameStates(&states[3], &states[4]);
-    expVal = 0;
-    if(compareInts(expVal, retVal) == 0){
-        printf("numHandCards(): PASS game state doesn't change after numHandCards() call\n");
+    actual = compareGameStates(&states[3], &states[4]);
+    expected = 0;
+    if(actual == expected){
+        printf("TEST 4: PASS game state doesn't change after numHandCards() call\n");
+        printf("\tgame state changed? = %d, expected = %d\n", actual, expected);
         numPasses++;
     }
     else{
-        printf("numHandCards(): FAIL game state does change after numHandCards() call\n");
+        printf("TEST 4: FAIL game state does change after numHandCards() call\n");
+        printf("\tgame state changed? = %d, expected = %d\n", actual, expected);
     }
 
     if(numPasses == 4){
