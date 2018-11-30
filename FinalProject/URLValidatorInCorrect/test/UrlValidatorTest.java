67c8b1b848487b57c69e87fba95ba2155a528f1b
Riley Kraft <kraftme@oregonstate.edu>
2018-11-30 10:32:47 -0500

    final project

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -18,18 +18,113 @@ public class UrlValidatorTest extends TestCase {
    }
    
    public void testManualTest(){
-	   //You can use this function to implement your manual testing	   
+	   //You can use this function to implement your manual testing	 
+	UrlValidator urlVal = new UrlValidator(null,null,UrlValidator.ALLOW_ALL_SCHEMES);
+	String[] validURLS = {	"http://www.ed.gov",			//standard web address
+			   	"http://www.gov.uk",			//different top level domain
+			   	"http://www.oregonstate.edu",		//different top level domain
+			   	"http://www.amazon.com",		//different top level domain
+			   	"http://www.mail.google.com",		//standard web address
+			   	"http://mail.google.com",		//removed www.
+			   	"www.mail.google.com",			//removed protocol
+			   	"http://www.mail.google.com/mail",	//added path
+			   	"http://www.mail.google.com/mail/u",	//additional directory
+			   	"https://mail.google.com",		//protocol change
+			   	"http://www.mail.google.com:55443"	//added port number
+	   };
+	  
+	 String[] invalidURLS = {"ioce;\\mmm.sometjh,cas/\"",		//clearly incorrect address
+			   	"mail.google.com",			//removed scheme
+			   	"http://www.wikipedia",			//removed top level domain
+			   	"http://.com",				//removed site name
+			   	"http://www.mail google.com",		//added space in address
+			   	"http;//www.mail.google.com",		//semi-colon after scheme
+			   	"http://www.",				//removed end of url
+			   	".com/path"				//removed first half of url
+	   };
+	   printTestResults(urlVal,validURLS,"Manual 1: Valid URLS");
+	   printTestResults(urlVal,invalidURLS, "Manual 2: Invalid URLS");    
 	   
    }
    
-   public void testYourFirstPartition(){
-	   //You can use this function to implement your First Partition testing	   
+   /*
+    * Used to test authority(2nd part) of url string
+    */
+   public void testYourAuthorityPartition(){
+		 //Url composition: scheme + authority + port + path + query
+		   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);  
+		   String[] testCases = {	"http://www.google",		//false
+				   					"http://",					//false
+				   					"http://0.0.0.0", 			//true
+				   					"http://255.255.255.255",	//true
+				   					"http://256.256.255.255",	//false
+				   					"http://192.168",			//false
+				   					"http://192.168.3.4.5",
+		   						};
+		   printTestResults(urlVal, testCases, "Partition 2: authority tests");
    }
    
-   public void testYourSecondPartition(){
-	   //You can use this function to implement your Second Partition testing	   
+   /*
+    * Used to test port(3rd part) of url string
+    */
+   public void testYourPortPartition(){
+		 //Url composition: scheme + authority + port + path + query
+		   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);  
+		   String[] testCases = {	"http://www.google.com:80", 
+				   					"http://www.google.com:0", 
+				   					"http://www.google.com:65535",
+				   					"http://www.google.com:65536",
+				   					"http://www.google.com:66000", 
+				   					"http://www.google.com:port", 
+		   						};
+		   printTestResults(urlVal, testCases, "Partition 3: port tests");
    }
+ 
+   /*
+    * Used to test path(4th part) of url string
+    */
+   public void testYourPathPartition(){
+		 //Url composition: scheme + authority + port + path + query
+		   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);  
+		   String[] testCases = {	"http://www.google.com/path", 
+				   					"http://www.google.com/path1/path2", 
+				   					"http://www.google.com/path1//path2", 
+				   					"http://www.google.com/..",
+		   						};
+		   printTestResults(urlVal, testCases, "Partition 4: path tests");
+   }
+   
+   /*
+    * Used to test query(5th part) of url string
+    */
+   public void testYourQueryPartition(){
+		 //Url composition: scheme + authority + port + path + query
+		   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);  
+		   String[] testCases = {	"http://www.google.com/path?action=view", 
+				   					"http://www.google.com/path?action=view&submit=yes",
+				   					"http://www.google.com/path?action$view",
+				   					"http://www.google.com/path?",
+		   						};
+		   printTestResults(urlVal, testCases, "Partition 5: query tests");
+   }
+   
    //You need to create more test cases for your Partitions if you need to 
+   public void printTestResults(UrlValidator urlVal, String[] testCases, String title) {
+	   boolean validUrl = true;
+	   System.out.print("\n" + title + "\n");
+	   System.out.print("Valid?\tURL\n");
+	   for(int i = 0; i < testCases.length; i++) {	
+		   try {
+			   validUrl = urlVal.isValid(testCases[i]);
+		   }
+		   catch(Exception | Error e) {
+			   System.out.println("FAIL: "+testCases[i]+" caused "+e);
+		   }
+			
+			System.out.print(validUrl + "\t");   
+		    System.out.print(testCases[i] + "\n");	
+	   }
+   } 
    
    public void testIsValid(){
 	   PrintWriter file = null;
